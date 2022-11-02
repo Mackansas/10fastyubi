a3f3238bc3868bf73fd157aa49fbaee001fe0316
Marcus Dahl <marcus-dahl@hotmail.com>
2022-11-02 12:46:19 +0100

    Daku + Const

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import {hiraDict, romajiToHira, hiraToRomaji} from './consts.js' 
 const hiraDisplayElement = document.getElementById('charDisplay')
 const inputElement = document.getElementById('inputBox')
 let endless = false
@@ -7,43 +8,9 @@ let hiragana = [
     'ね','の','は','ひ','ふ','へ','ほ','ま','み','む','め','も','や','ゆ','よ','ら','り','る','れ','ろ','わ','を','ん'
     ]
 
-const hiraDict = {
-    'vHira' : ['あ','い','う','え','お'], 'kHira' : ['か','き','く','け','こ'], 'sHira' : ['さ','し','す','せ','そ'],
-    'tHira' : ['た','ち','つ','て','と'], 'nHira' : ['な','に','ぬ','ね','の','ん'], 'hHira' : ['は','ひ','ふ','へ','ほ'],
-    'mHira' : ['ま','み','む','め','も'], 'yHira' : ['や','ゆ','よ'], 'rHira' : ['ら','り','る','れ','ろ'], 
-    'wHira' : ['わ','を']
-}
-
-const romajiToHira = {
-    'wo':'を','wa':'わ',
-    'ro':'ろ','re':'れ','ru':'る','ri':'り','ra':'ら',
-    'yo':'よ','yu':'ゆ','ya':'や',
-    'mo':'も','me':'め','mu':'む','mi':'み','ma':'ま',
-    'ho':'ほ','he':'へ','fu':'ふ','hi':'ひ','ha':'は',
-    'no':'の','ne':'ね','nu':'ぬ','ni':'に','na':'な',
-    'to':'と','te':'て','tsu':'つ','chi':'ち','ta':'た',
-    'so':'そ','se':'せ','su':'す','shi':'し','sa':'さ',
-    'ko':'こ','ke':'け','ku':'く','ki':'き','ka':'か',
-    'n':'ん','o':'お','e':'え','u':'う','i':'い','a':'あ'
-}
-
-const hiraToRomaji = {
-    'あ':'a','い':'i','う':'u','え':'e','お':'o','ん':'n',
-    'か':'ka','き':'ki','く':'ku','け':'ke','こ':'ko',
-    'さ':'sa','し':'shi','す':'su','せ':'se','そ':'so',
-    'た':'ta','ち':'chi','つ':'tsu','て':'te','と':'to',
-    'な':'na','に':'ni','ぬ':'nu','ね':'ne','の':'no',
-    'は':'ha','ひ':'hi','ふ':'fu','へ':'he','ほ':'ho',
-    'ま':'ma','み':'mi','む':'mu','め':'me','も':'mo',
-    'や':'ya','ゆ':'yu','よ':'yo',
-    'ら':'ra','り':'ri','る':'ru','れ':'re','ろ':'ro',
-    'わ':'wa','を':'wo'
-}
-
-const hiraButtonsID = ['vHira','kHira','sHira','tHira','nHira','hHira','mHira','yHira','rHira','wHira']
+const hiraButtonsID = ['vHira','kHira','sHira','tHira','nHira','hHira','mHira','yHira','rHira','wHira', 'dHira']
 
 function renderNextHiragana() {
-    // const hiragana = ['あ','い','う','え','お']
     return (hiragana[Math.floor(Math.random() * hiragana.length)])
 }
 
